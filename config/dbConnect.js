7085c9d095fc897c5421958f9ae3e0ab1427b070
tommywa-a <tomiwa95@gmail.com>
2023-05-14 15:37:10 +0100

    database schema

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require('mongoose')
 
 const dbConnect = () => {
 	try {
-		const conn = mongoose.connect('mongodb://localhost:27017/ecommerce-app')
+		const conn = mongoose.connect(process.env.MONGODB_URL)
     console.log('Database connected successfully')
 	} catch (error) {
 		console.log('Database error')
