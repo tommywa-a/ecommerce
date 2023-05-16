c603acb8d288ae750c4bc19b9401a07fa3773758
tommywa-a <tomiwa95@gmail.com>
2023-05-16 09:27:15 +0100

    modified userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,42 +1,53 @@
-const mongoose = require('mongoose'); // Erase if already required
+const mongoose = require('mongoose') // Erase if already required
 const bcrypt = require('bcrypt')
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
-    },
-    lastName:{
-        type:String,
-        required:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-    role: {
-      type: String,
-      default: "user",
-    },
-});
+var userSchema = new mongoose.Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		mobile: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		role: {
+			type: String,
+			default: 'user',
+		},
+		cart: {
+			type: Array,
+			default: [],
+		},
+		address: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Address' }],
+		wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+	},
+	{
+		timestamps: true,
+	}
+)
 
 userSchema.pre('save', async function (next) {
-  const salt = await bcrypt.genSaltSync(10);
-  this.password = await bcrypt.hash(this.password, salt)
+	const salt = await bcrypt.genSaltSync(10)
+	this.password = await bcrypt.hash(this.password, salt)
 })
 userSchema.methods.isPasswordMatched = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword,this.password)
+	return await bcrypt.compare(enteredPassword, this.password)
 }
 
 //Export the model
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', userSchema)
