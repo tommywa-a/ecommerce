e7dcac384b90b67a2a27e100c06362a9c32b1f55
tommywa-a <tomiwa95@gmail.com>
2023-05-17 15:09:21 +0100

    accessToken

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -3,6 +3,7 @@ const User = require('../models/userModel')
 const asyncHandler = require('express-async-handler')
 const validateMongoDBID = require('../utils/validateMongodbID')
 const { generateRefreshToken } = require('../config/refreshToken')
+const jwt = require('jsonwebtoken')
 const createUser = asyncHandler(async (req, res) => {
 	const email = req.body.email
 	const findUser = await User.findOne({ email })
@@ -22,16 +23,18 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
 	// check if user exists
 	const findUser = await User.findOne({ email })
 	if (findUser && (await findUser.isPasswordMatched(password))) {
-    const refreshToken = await generateRefreshToken(findUser?._id)
-    const updateuser = await User.findByIdAndUpdate(findUser.id, {
-      refreshToken: refreshToken,
-    },
-    {new: true}
-    )
-    res.cookie('refreshToken', refreshToken, {
-      httpOnly: true,
-      maxAge: 3*24*60*60*1000,
-     })
+		const refreshToken = await generateRefreshToken(findUser?._id)
+		const updateuser = await User.findByIdAndUpdate(
+			findUser.id,
+			{
+				refreshToken: refreshToken,
+			},
+			{ new: true }
+		)
+		res.cookie('refreshToken', refreshToken, {
+			httpOnly: true,
+			maxAge: 3 * 24 * 60 * 60 * 1000,
+		})
 		res.json({
 			_id: findUser?._id,
 			firstName: findUser?.firstName,
@@ -45,115 +48,139 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
 })
 
 // Handle refresh token
-const handleRefreshToken = asyncHandler(async (req,res) => {
-  const cookie = req.cookies
-  if (!cookie?.refreshToken) {
-    throw new Error("No Refresh Token in Cookies")
-  }
-  const refreshToken = cookie.refreshToken
-  console.log(refreshToken)
-  const user = await User.findOne({refreshToken})
-  if (!user) {
-    throw new Error("No refresh token matched with user in database")
-  }
-  res.json(user)
+const handleRefreshToken = asyncHandler(async (req, res) => {
+	const cookie = req.cookies
+	if (!cookie?.refreshToken) {
+		throw new Error('No Refresh Token in Cookies')
+	}
+	const refreshToken = cookie.refreshToken
+	const user = await User.findOne({ refreshToken })
+	if (!user) {
+		throw new Error('No refresh token matched with user in database')
+	}
+	jwt.verify(refreshToken, process.env.JWT_SECRET, (err, decoded) => {
+		if (err || user.id !== decoded.id) {
+			throw new Error('There is something wrong with the refresh token')
+		}
+		const accessToken = generateToken(user?._id)
+		res.json({ accessToken })
+	})
 })
 
 // Update a user
 const updateAUser = asyncHandler(async (req, res) => {
-  const {_id} = req.user
-  validateMongoDBID(_id)
-  try {
-    const updatedUser = await User.findByIdAndUpdate(_id, {
-      firstName: req?.body?.firstName,
-      lastName: req?.body?.lastName,
-      email: req?.body?.email,
-      mobile: req?.body?.mobile,
-    }, {
-      new: true,
-    })
-    res.json(updatedUser)
-  } catch (error) {
-    throw new Error(error)
-  }
+	const { _id } = req.user
+	validateMongoDBID(_id)
+	try {
+		const updatedUser = await User.findByIdAndUpdate(
+			_id,
+			{
+				firstName: req?.body?.firstName,
+				lastName: req?.body?.lastName,
+				email: req?.body?.email,
+				mobile: req?.body?.mobile,
+			},
+			{
+				new: true,
+			}
+		)
+		res.json(updatedUser)
+	} catch (error) {
+		throw new Error(error)
+	}
 })
 
 // Get all users
 
 const getAllUsers = asyncHandler(async (req, res) => {
-  try {
-    const getUsers= await User.find()
-    res.json(getUsers)
-  } catch (error) {
-    throw new Error(error)
-  }
+	try {
+		const getUsers = await User.find()
+		res.json(getUsers)
+	} catch (error) {
+		throw new Error(error)
+	}
 })
 
 // Get a single user
 
-const getAUser = asyncHandler(async(req, res) => {
-  const {id} = req.params
-  validateMongoDBID(id)
-  try {
-    const getaUser = await User.findById(id)
-    res.json({
-      getaUser
-    })
-  } catch (error) {
-    throw new Error(error)
-  }
+const getAUser = asyncHandler(async (req, res) => {
+	const { id } = req.params
+	validateMongoDBID(id)
+	try {
+		const getaUser = await User.findById(id)
+		res.json({
+			getaUser,
+		})
+	} catch (error) {
+		throw new Error(error)
+	}
 })
 
 // Delete a user
 
-const deleteAUser = asyncHandler(async(req, res) => {
-  const {id} = req.params
-  validateMongoDBID(_id)
-  try {
-    const deletedUser = await User.findByIdAndDelete(id)
-    res.json({
-      deletedUser
-    })
-  } catch (error) {
-    throw new Error(error)
-  }
+const deleteAUser = asyncHandler(async (req, res) => {
+	const { id } = req.params
+	validateMongoDBID(_id)
+	try {
+		const deletedUser = await User.findByIdAndDelete(id)
+		res.json({
+			deletedUser,
+		})
+	} catch (error) {
+		throw new Error(error)
+	}
 })
 
-const blockUser= asyncHandler(async (req, res) => {
-  const {id} = req.params
-  validateMongoDBID(id)
-  try {
-    const block = await User.findByIdAndUpdate(id, {
-      isBlocked: true
-    },
-    {
-      new: true
-    })
-    res.json({
-      message: "User blocked"
-    })
-  } catch (error) {
-    throw new Error(error)
-  }
+const blockUser = asyncHandler(async (req, res) => {
+	const { id } = req.params
+	validateMongoDBID(id)
+	try {
+		const block = await User.findByIdAndUpdate(
+			id,
+			{
+				isBlocked: true,
+			},
+			{
+				new: true,
+			}
+		)
+		res.json({
+			message: 'User blocked',
+		})
+	} catch (error) {
+		throw new Error(error)
+	}
 })
 
-const unblockUser= asyncHandler(async (req, res) => {
-  const {id} = req.params
-  validateMongoDBID(id)
-  try {
-    const unblock = await User.findByIdAndUpdate(id, {
-      isBlocked: false
-    },
-    {
-      new: true
-    })
-    res.json({
-      message: "User unblocked"
-    })
-  } catch (error) {
-    throw new Error(error)
-  }
+const unblockUser = asyncHandler(async (req, res) => {
+	const { id } = req.params
+	validateMongoDBID(id)
+	try {
+		const unblock = await User.findByIdAndUpdate(
+			id,
+			{
+				isBlocked: false,
+			},
+			{
+				new: true,
+			}
+		)
+		res.json({
+			message: 'User unblocked',
+		})
+	} catch (error) {
+		throw new Error(error)
+	}
 })
 
-
-module.exports = { createUser, loginUserCtrl, getAllUsers, getAUser, deleteAUser, updateAUser, blockUser, unblockUser, handleRefreshToken }
+module.exports = {
+	createUser,
+	loginUserCtrl,
+	getAllUsers,
+	getAUser,
+	deleteAUser,
+	updateAUser,
+	blockUser,
+	unblockUser,
+	handleRefreshToken,
+}
