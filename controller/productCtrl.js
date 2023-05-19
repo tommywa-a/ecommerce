5777518705ea8f3f37ee06b28c1fd76f3b42a62c
tommywa-a <tomiwa95@gmail.com>
2023-05-19 23:42:56 +0100

    modified productCtrl.js

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,24 +1,23 @@
 const Product = require('../models/productModel')
 const asyncHandler = require('express-async-handler')
 
-const createProduct = asyncHandler(async(req, res) => {
-
-  try {
-    const newProduct = await Product.create(req.body)
-    res.json(newProduct)  
-  } catch (error) {
-    throw new Error(error)
-  }
+const createProduct = asyncHandler(async (req, res) => {
+	try {
+		const newProduct = await Product.create(req.body)
+		res.json(newProduct)
+	} catch (error) {
+		throw new Error(error)
+	}
 })
 
 const getAProduct = asyncHandler(async (req, res) => {
-  const {id} = req.params
-  try {
-    const findProduct = await Product.findById(id)
-    res.json(findProduct)
-  } catch (error) {
-    throw new Error(error)
-  }
+	const { id } = req.params
+	try {
+		const findProduct = await Product.findById(id)
+		res.json(findProduct)
+	} catch (error) {
+		throw new Error(error)
+	}
 })
 
-module.exports = {createProduct, getAProduct}
+module.exports = { createProduct, getAProduct }
