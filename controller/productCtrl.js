d57bd393aee1f5428e00903fda31d722ae7787ee
tommywa-a <tomiwa95@gmail.com>
2023-05-19 23:40:20 +0100

    get a product

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -9,7 +9,16 @@ const createProduct = asyncHandler(async(req, res) => {
   } catch (error) {
     throw new Error(error)
   }
-  
 })
 
-module.exports = {createProduct}
+const getAProduct = asyncHandler(async (req, res) => {
+  const {id} = req.params
+  try {
+    const findProduct = await Product.findById(id)
+    res.json(findProduct)
+  } catch (error) {
+    throw new Error(error)
+  }
+})
+
+module.exports = {createProduct, getAProduct}
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { createProduct } = require('../controller/productCtrl')
+const { createProduct, getAProduct } = require('../controller/productCtrl')
 const router = express.Router()
 
 router.post('/', createProduct)
+router.get('/:id', getAProduct)
 
 module.exports = router
