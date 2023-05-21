a95d8b7d20e8d6c52aa91bb0727f8639997055a4
tommywa-a <tomiwa95@gmail.com>
2023-05-21 23:55:10 +0100

    implemented Slugify package

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,8 +1,12 @@
 const Product = require('../models/productModel')
 const asyncHandler = require('express-async-handler')
+const slugify = require('slugify')
 
 const createProduct = asyncHandler(async (req, res) => {
 	try {
+		if(req.body.title) {
+			req.body.slug=slugify(req.body.title)
+		}
 		const newProduct = await Product.create(req.body)
 		res.json(newProduct)
 	} catch (error) {
