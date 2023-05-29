b40774d4c777b2bbc471e28e147428d2389365b0
tommywa-a <tomiwa95@gmail.com>
2023-05-29 23:54:54 +0100

    Implemented sorting

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -51,13 +51,25 @@ const getAProduct = asyncHandler(async (req, res) => {
 
 const getAllproducts = asyncHandler(async (req, res) => {
 	try {
+
+		// Filtering
 		const queryObj = {...req.query}
 		const excludeFields = ['page', 'sort', 'limit', 'fields']
 		excludeFields.forEach(el => delete queryObj[el])
 		let queryStr = JSON.stringify(queryObj)
 		queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
 		
-		const query = Product.find(JSON.parse(queryStr))
+		let query = Product.find(JSON.parse(queryStr))
+
+		// Sorting
+
+		if (req.query.sort) {const sortBy = req.query.sort.split(',').join(" ")
+			query = query.sort(sortBy)
+			
+		} else {
+			query= query.sort('-createdAt')
+		}
+
 		const product = await query
 		res.json(product)
 	} catch (error) {
