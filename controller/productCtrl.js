dde023fa5945fc1a8c07ce8b390553ea7e25994d
tommywa-a <tomiwa95@gmail.com>
2023-05-30 12:15:55 +0100

    Implementing Pagination

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -79,6 +79,20 @@ const getAllproducts = asyncHandler(async (req, res) => {
 			query = query.select('-__v')
 		}
 
+		// Pagination
+
+		const page = req.query.page
+		const limit = req.query.limit
+		const skip = (page - 1) * limit
+		query = query.skip(skip).limit(limit)
+		if (req.query.page) {
+			const productCount = await Product.countDocuments()
+			if (skip>=productCount) {
+				throw new Error('This page does not exist')
+			}
+		}
+		console.log(page, limit, skip)
+
 		const product = await query
 		res.json(product)
 	} catch (error) {
