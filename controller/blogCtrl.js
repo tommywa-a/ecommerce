233637a14787edab8c327e581fab5e680488b258
tommywa-a <tomiwa95@gmail.com>
2023-06-04 13:16:56 +0100

    createBlog functionality

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -4,6 +4,13 @@ const asyncHandler = require('express-async-handler')
 const validateMongoDBID = require('../utils/validateMongodbID')
 
 
-const createBlog = asyncHandler(async (req, res) => {})
+const createBlog = asyncHandler(async (req, res) => {
+  try {
+    const newBlog = await Blog.create(req.body)
+    res.json(newBlog)
+  } catch (error) {
+    throw new Error(error)
+  }
+})
 
 module.exports = {createBlog}
