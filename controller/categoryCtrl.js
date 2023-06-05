b92530396e34eb6100c33dfada1c7e2df8c85be0
tommywa-a <tomiwa95@gmail.com>
2023-06-05 23:01:19 +0100

    Creating a new category

---
+++ controller/categoryCtrl.js
@@ -0,0 +1,14 @@
+const Category = require('../models/categoryModel')
+const asyncHandler = require('express-async-handler')
+const validateMongoDBID = require('../utils/validateMongodbID')
+
+const createCategory = asyncHandler(async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body)
+    res.json(newCategory)
+  } catch (error) {
+    throw new Error(error)
+  }
+})
+
+module.exports = { createCategory }
