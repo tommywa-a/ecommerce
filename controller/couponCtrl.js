534811c330559d9977ad8430a75eb7f8b2f7c024
tommywa-a <tomiwa95@gmail.com>
2023-06-12 11:14:06 +0100

    updateCoupon functionality

--- controller/couponCtrl.js
+++ controller/couponCtrl.js
@@ -2,22 +2,35 @@ const Coupon = require('../models/couponModel')
 const validateMongoDBID = require('../utils/validateMongodbID')
 const asyncHandler = require('express-async-handler')
 
-const createCoupon = asyncHandler(async(req,res)=> {
-  try {
-    const newCoupon = await Coupon.create(req.body)
-    res.json(newCoupon)
-  } catch (error) {
-    throw new Error(error)
-  }
+const createCoupon = asyncHandler(async (req, res) => {
+	try {
+		const newCoupon = await Coupon.create(req.body)
+		res.json(newCoupon)
+	} catch (error) {
+		throw new Error(error)
+	}
 })
 
-const getAllCoupons = asyncHandler(async(req,res)=> {
-  try {
-    const coupons = await Coupon.find()
-    res.json(coupons)
-  } catch (error) {
-    throw new Error(error)
-  }
+const getAllCoupons = asyncHandler(async (req, res) => {
+	try {
+		const coupons = await Coupon.find()
+		res.json(coupons)
+	} catch (error) {
+		throw new Error(error)
+	}
 })
 
-module.exports = {createCoupon, getAllCoupons}
+const updateCoupon = asyncHandler(async (req, res) => {
+	const { id } = req.params
+	validateMongoDBID(id)
+	try {
+		const updatecoupon = await Coupon.findByIdAndUpdate(id, req.body, {
+			new: true,
+		})
+		res.json(updatecoupon)
+	} catch (error) {
+		throw new Error(error)
+	}
+})
+
+module.exports = { createCoupon, getAllCoupons, updateCoupon }
