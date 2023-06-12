ba03143714bf4a828d544bc477bf99196a195b93
tommywa-a <tomiwa95@gmail.com>
2023-06-12 11:21:11 +0100

    deleteCoupon functionality

--- controller/couponCtrl.js
+++ controller/couponCtrl.js
@@ -2,35 +2,44 @@ const Coupon = require('../models/couponModel')
 const validateMongoDBID = require('../utils/validateMongodbID')
 const asyncHandler = require('express-async-handler')
 
-const createCoupon = asyncHandler(async (req, res) => {
-	try {
-		const newCoupon = await Coupon.create(req.body)
-		res.json(newCoupon)
-	} catch (error) {
-		throw new Error(error)
-	}
+const createCoupon = asyncHandler(async(req,res)=> {
+  try {
+    const newCoupon = await Coupon.create(req.body)
+    res.json(newCoupon)
+  } catch (error) {
+    throw new Error(error)
+  }
 })
 
-const getAllCoupons = asyncHandler(async (req, res) => {
-	try {
-		const coupons = await Coupon.find()
-		res.json(coupons)
-	} catch (error) {
-		throw new Error(error)
-	}
+const getAllCoupons = asyncHandler(async(req,res)=> {
+  try {
+    const coupons = await Coupon.find()
+    res.json(coupons)
+  } catch (error) {
+    throw new Error(error)
+  }
 })
 
-const updateCoupon = asyncHandler(async (req, res) => {
-	const { id } = req.params
-	validateMongoDBID(id)
-	try {
-		const updatecoupon = await Coupon.findByIdAndUpdate(id, req.body, {
-			new: true,
-		})
-		res.json(updatecoupon)
-	} catch (error) {
-		throw new Error(error)
-	}
+const updateCoupon = asyncHandler(async(req,res)=> {
+  const {id} = req.params
+  validateMongoDBID(id)
+  try {
+    const updatecoupon = await Coupon.findByIdAndUpdate(id, req.body, {new:true})
+    res.json(updatecoupon)
+  } catch (error) {
+    throw new Error(error)
+  }
 })
 
-module.exports = { createCoupon, getAllCoupons, updateCoupon }
+const deleteCoupon = asyncHandler(async(req,res)=> {
+  const {id} = req.params
+  validateMongoDBID(id)
+  try {
+    const deletecoupon = await Coupon.findByIdAndDelete(id)
+    res.json(deletecoupon)
+  } catch (error) {
+    throw new Error(error)
+  }
+})
+
+module.exports = {createCoupon, getAllCoupons, updateCoupon, deleteCoupon}
