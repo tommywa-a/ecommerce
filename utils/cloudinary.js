98bdba3011089aee7afb6e62a95cde3b9c524ef7
tommywa-a <tomiwa95@gmail.com>
2023-06-13 22:49:04 +0100

    cloudinary configuration

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,6 +1,24 @@
 const cloudinary = require('cloudinary')
-cloudinary.config({ 
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.API_KEY, 
-    api_secret: process.env.SECRET_KEY,
-  })
+
+cloudinary.config({
+	cloud_name: process.env.CLOUD_NAME,
+	api_key: process.env.API_KEY,
+	api_secret: process.env.SECRET_KEY,
+})
+
+const cloudinaryUploadImg = async (fileToUpload) => {
+	return new Promise((resolve) => {
+		cloudinary.UploadStream.upload(fileToUpload, (result) => {
+			resolve(
+				{
+					url: result.secure_url,
+				},
+				{
+					resource_type: 'auto',
+				}
+			)
+		})
+	})
+}
+
+module.exports = cloudinaryUploadImg
