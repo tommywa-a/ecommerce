6b6b0bed0d217fd625fbfc656eec2a0a645d6f25
tommywa-a <tomiwa95@gmail.com>
2023-06-20 14:09:26 +0100

    finished uploadImages functionality for blogs

--- controller/blogCtrl.js
+++ controller/blogCtrl.js
@@ -2,6 +2,8 @@ const Blog = require('../models/blogModel')
 const User = require('../models/userModel')
 const asyncHandler = require('express-async-handler')
 const validateMongoDBID = require('../utils/validateMongodbID')
+const cloudinaryUploadImg = require('../utils/cloudinary')
+const fs = require('fs')
 
 const createBlog = asyncHandler(async (req, res) => {
 	try {
@@ -161,6 +163,36 @@ const dislikeTheBlog = asyncHandler(async (req, res) => {
 	}
 })
 
+const uploadImages = asyncHandler(async (req, res) => {
+	const { id } = req.params
+	validateMongoDBID(id)
+	try {
+		const uploader = (path) => cloudinaryUploadImg(path, 'images')
+		const urls = []
+		const files = req.files
+		for (const file of files) {
+			const { path } = file
+			const newpath = await uploader(path)
+			urls.push(newpath)
+			fs.unlinkSync(path)
+		}
+		const findBlog = await Blog.findByIdAndUpdate(
+			id,
+			{
+				images: urls.map((file) => {
+					return file
+				}),
+			},
+			{
+				new: true,
+			}
+		)
+		res.json(findBlog)
+	} catch (error) {
+		throw new Error(error)
+	}
+})
+
 module.exports = {
 	createBlog,
 	updateBlog,
@@ -169,4 +201,5 @@ module.exports = {
 	deleteBlog,
 	likeTheBlog,
 	dislikeTheBlog,
+	uploadImages,
 }
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware')
-const { createBlog, updateBlog, getBlog, getAllBlogs, deleteBlog, likeTheBlog, dislikeTheBlog } = require('../controller/blogCtrl')
+const {
+	createBlog,
+	updateBlog,
+	getBlog,
+	getAllBlogs,
+	deleteBlog,
+	likeTheBlog,
+	dislikeTheBlog,
+  uploadImages,
+} = require('../controller/blogCtrl')
+const { blogImgResize, uploadPhoto } = require('../middlewares/uploadImages')
 const router = express.Router()
 
 router.get('/:id', getBlog)
@@ -8,6 +18,14 @@ router.get('/', getAllBlogs)
 router.put('/likes', authMiddleware, likeTheBlog)
 router.put('/dislikes', authMiddleware, dislikeTheBlog)
 router.post('/', authMiddleware, isAdmin, createBlog)
+router.put(
+	'/upload/:id',
+	authMiddleware,
+	isAdmin,
+  uploadPhoto.array('images', 2),
+	blogImgResize,
+	uploadImages
+)
 router.put('/:id', authMiddleware, isAdmin, updateBlog)
 router.delete('/:id', authMiddleware, isAdmin, deleteBlog)
 
