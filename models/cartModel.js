790c9af009fff49c3c431147fc3b22b09aeca01d
tommywa-a <tomiwa95@gmail.com>
2023-07-01 23:36:16 +0100

    finished cartModel.js

--- models/cartModel.js
+++ models/cartModel.js
@@ -1,1 +1,30 @@
+const mongoose = require('mongoose'); // Erase if already required
 
+// Declare the Schema of the Mongo model
+var cartSchema = new mongoose.Schema(
+	{
+		products: [
+			{
+				product: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: 'Product',
+				},
+				count: Number,
+				colour: String,
+        Price: Number,
+			},
+		],
+		cartTotal: Number,
+		totalAfterDiscount: Number,
+		orderby: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+//Export the model
+module.exports = mongoose.model('Cart', cartSchema);
